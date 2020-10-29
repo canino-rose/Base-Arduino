13f430399dba72d3c1d2e6a34f5a38b5098df598
Canino <canino@rose-hulman.edu>
2020-10-29 14:01:06 -0400

    Arduino Base Code

---
+++ Arduino_Base_Template.ino
@@ -0,0 +1,19 @@
+// select programmer (AVR ISP)
+// select board type
+// select COM port
+//setup a global here
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+// void function() {} // Create your own function
